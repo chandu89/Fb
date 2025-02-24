9e7c5965701e5d63b855775e6b8353a7f0cf0510
chandanchoubey <vibhatsu.choubey@gmail.com>
2025-02-24 17:47:14 +0100

    migrate command removed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,3 @@ bundle exec rails assets:clean
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
-
-bundle exec rails db:migrate
