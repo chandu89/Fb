1d23776ad7cae31f347502ee40991f5bd493c2c9
chandanchoubey <vibhatsu.choubey@gmail.com>
2025-02-24 15:08:15 +0100

    Search functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
       format.json { render json: { results: @users.map { |user| {user_id: user.id, first_name: user.first_name, last_name: user.last_name } } } }
     end
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  
+    if @user
+      @posts = @user.posts # Fetch posts for the user
+    else
+      flash[:alert] = "User not found"
+      redirect_to users_path # Redirect if the user is not found
+    end
+  end  
+  
 end
