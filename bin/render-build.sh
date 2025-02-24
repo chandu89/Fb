de1dd525e2665852510cc18a2781aadaeab525b1
chandanchoubey <vibhatsu.choubey@gmail.com>
2025-02-24 18:21:07 +0100

    render.sh changes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,3 +9,5 @@ bundle exec rails assets:clean
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
 # Uncomment the following line:
+puts "---Chandan----"
+bundle exec rails db:migrate
